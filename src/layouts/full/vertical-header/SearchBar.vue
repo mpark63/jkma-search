63111c844cde56792a730af8d0524223b49d03f6
mpark63 <minspk13@gmail.com>
2024-04-17 23:01:40 -0400

    Implement routing to search page

---
+++ src/layouts/full/vertical-header/SearchBar.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+import { router } from '@/router';
+import { SearchIcon } from 'vue-tabler-icons';
+
+const query = ref('');
+
+function search(query) {
+  if (query) {
+    router.push('/search/' + query);
+  }
+}
+</script>
+
+<template>
+  <!-- ---------------------------------------------- -->
+  <!-- searchbar -->
+  <!-- ---------------------------------------------- -->
+  <v-text-field v-model="query" persistent-placeholder placeholder="Search" color="primary" variant="outlined" hide-details>
+    <template v-slot:append-inner>
+      <v-btn color="lightsecondary" icon rounded="sm" variant="flat" size="small" class="text-secondary SearchSetting">
+        <SearchIcon stroke-width="1.5" size="17" class="text-lightText SearchIcon" @click="search(query)" />
+      </v-btn>
+    </template>
+  </v-text-field>
+</template>
---
+++ src/layouts/full/vertical-header/SearchHistory.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- ---------------------------------------------- -->
+  <!-- profile DD -->
+  <!-- ---------------------------------------------- -->
+  <div class="pa-4">
+    <h4 class="mb-n1 my-3">Search history</h4>
+    <v-divider></v-divider>
+    <perfect-scrollbar style="height: calc(100vh - 300px); max-height: 515px">
+      <v-list class="mt-3">
+        <v-list-item color="secondary" rounded="md">
+          <v-list-item-title class="text-subtitle-2"> heller</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item color="secondary" rounded="md">
+          <v-list-item-title class="text-subtitle-2"> heller</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item color="secondary" rounded="md">
+          <v-list-item-title class="text-subtitle-2"> heller</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </perfect-scrollbar>
+  </div>
+</template>
