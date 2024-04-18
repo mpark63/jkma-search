0d4ac1aeacf53f609aa3784a3c670a927fa3f11f
mpark63 <minspk13@gmail.com>
2024-04-17 23:43:58 -0400

    Update readme and home page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,20 +1,28 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 import UiParentCard from '@/components/shared/UiParentCard.vue';
 import TitleBar from '@/components/shared/TitleBar.vue';
-
-const page = ref({ title: 'Home' });
 </script>
 
 <template>
-  <TitleBar :title="page.title"></TitleBar>
-  <v-row>
-    <v-col cols="12" md="12">
-      <UiParentCard title="Simple Title">
-        Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-        minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in
-        reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui
-        officiate descent molls anim id est labours.
+  <v-row class="d-flex justify-center align-center">
+    <v-col cols="12" md="8">
+      <TitleBar title="Home"></TitleBar>
+      <UiParentCard title="About" class="mb-2">
+        This website is an unofficial search engine for the Journal of Korean Medical Association. It is to help with topic development for
+        Cross Lingual Information Retrieval research under Dr. Kenton Murray and Dr. Eugene Yang. We received permission from the Korean
+        Medical Association to use their data for research purposes.
+      </UiParentCard>
+      <UiParentCard title="Getting started" class="mb-2">
+        You can start by searching for a keyword in the search bar above. The search engine will return a list of articles that contain the
+        most relevant information. You can click on an article in the search results and view the most relevant page of the article. For
+        more instructions on the topic development task, please refer to the
+        <a href="https://docs.google.com/document/d/1daMyY1Ez7BuLse2A5AY75PFurxLn_h225-SekkKg_7Y/edit?usp=sharing" target="_blank">
+          Korean-English Topic Development Official Guide
+        </a>
+        .
+      </UiParentCard>
+      <UiParentCard title="Questions?">
+        Feel free to contact <a href="mailto:mpark63@jh.edu">mpark63@jh.edu</a> with any questions.
       </UiParentCard>
     </v-col>
   </v-row>
